09df2e6d4e4c78654673e3180792957aff0ebe6e
M. Hasan <mohiuddinkhasherhat@gmail.com>
2023-05-04 13:51:36 +0600

    Added Filter feature on the recipies page + Favorite racipies

--- src/Components/Pages/Recipies.jsx
+++ src/Components/Pages/Recipies.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
-import { HandThumbUpIcon, HeartIcon } from '@heroicons/react/24/solid';
+import { ChevronDownIcon, HandThumbUpIcon, HeartIcon } from '@heroicons/react/24/solid';
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { addToFavorite } from '../../utilities/utilities';
 import SpinnerBar from '../Elements/SpinnerBar';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Link } from 'react-router-dom';
 
 const Recipies = () => {
     const [allRecipies, setAllRecipies] = useState([]);
     const [savedRecipies, setSavedRecipies] = useState([]);
+    const [selectedChef, setSelectedChef] = useState(null);
+    const [selectedRecipe, setSelectedRecipe] = useState([]);
 
     const notify = (message) => toast(message);
 
@@ -19,6 +22,7 @@ const Recipies = () => {
         .then(res => res.json())
         .then(data => {
             setAllRecipies(data);
+            setSelectedChef("All")
             let localRecipies = localStorage.getItem("recipies");
                 if (localRecipies != null) {
                     setSavedRecipies(JSON.parse(localRecipies))
@@ -26,8 +30,37 @@ const Recipies = () => {
         })
     },[])
 
+    const optionSelect = (e) => {
+        document.querySelector("#selectOutput").innerHTML = (e.target.getAttribute("data-value"));
+        document.querySelector("#selectOptions").classList.add("hidden");
+        document.querySelector("#selectIcon").classList.remove("rotate-180");
+        e.stopPropagation();
+        setSelectedChef(e.target.getAttribute("data-value"));
+    }
+    const optionToggle = () => {
+        document.querySelector("#selectOptions").classList.toggle("hidden");
+        document.querySelector("#selectIcon").classList.toggle("rotate-180");
+    }
+
+    useEffect(()=>{
+        const selectedRecipes = allRecipies.filter(one=>one.chef_name.includes(selectedChef));
+        if (selectedChef == "All") {
+            setSelectedRecipe(allRecipies);
+        }else if (selectedChef == "Favorites") {
+            let localRecipe = [];
+            allRecipies.map(recipe=>{
+                if (savedRecipies.includes(recipe.id)) {
+                    localRecipe.push(recipe);
+                }
+            })
+            console.log(localRecipe);
+            setSelectedRecipe(localRecipe);
+        } else {
+            setSelectedRecipe(selectedRecipes);
+        }
+    },[allRecipies, savedRecipies, selectedChef])
+
     const RecipeLikeButtonClasses = (recipe) => {
-        console.log(savedRecipies);
         if (savedRecipies == []) {
             return "h-5 w-5 text-white cursor-pointer";
         } else {
@@ -51,56 +84,78 @@ const Recipies = () => {
     }
 
     return (
-        <div className='grid grid-cols-3 gap-4'>
-            {allRecipies.map(recipe=> {
-                return(
-                    <div key={recipe.id} className="border border-gray-300 rounded-lg overflow-hidden bg-white">
-                        <div className='relative overflow-hidden'>
-                            <img src={recipe.banner_image} className="h-32 w-full object-cover" />
-                            <div className='absolute bottom-0 bg-gradient-to-t from-black to-transparent px-24 py-12 rotate-45 -translate-x-16 translate-y-12'>
-                                <div title='Number of Likes' className="flex gap-1 items-center mt-12 -rotate-45">
-                                    <HandThumbUpIcon className='h-5 w-5 text-white' />
-                                    <p className='font-bold text-sm text-white'>{recipe.likes}</p>
+        <div>
+            <ToastContainer></ToastContainer>
+            <div className="flex justify-center gap-4 mb-8">
+                <p className='font-secondary text-2xl'>You Are Viewing:</p>
+                <div onClick={optionToggle} className='cursor-pointer relative w-40 bg-white border border-gray-200 grid place-content-center select-none'>
+                    <p id="selectOutput" className='pointer-events-none'>All</p>
+                    <ChevronDownIcon id="selectIcon" className='h-4 w-4 absolute right-2 top-1/2 -translate-y-1/2'></ChevronDownIcon>
+                    <div id="selectOptions" className='hidden absolute top-7 bg-white z-10 p-4 left-0 w-full border shadow-md flex flex-col gap-1'>
+                        <p data-value="All"onClick={optionSelect} className='cursor-pointer transition duration-300 hover:text-orange-400'>All</p>
+                        <p data-value="Favorites"onClick={optionSelect} className='cursor-pointer transition duration-300 hover:text-orange-400'>Favorites</p>
+                        <p data-value="Molly Smith"onClick={optionSelect} className='cursor-pointer transition duration-300 hover:text-orange-400'>Molly Smith</p>
+                        <p data-value="Juan Garcia"onClick={optionSelect} className='cursor-pointer transition duration-300 hover:text-orange-400'>Juan Garcia</p>
+                        <p data-value="Sophie Martin"onClick={optionSelect} className='cursor-pointer transition duration-300 hover:text-orange-400'>Sophie Martin</p>
+                        <p data-value="Antonio Rossi"onClick={optionSelect} className='cursor-pointer transition duration-300 hover:text-orange-400'>Antonio Rossi</p>
+                        <p data-value="Chen Liu"onClick={optionSelect} className='cursor-pointer transition duration-300 hover:text-orange-400'>Chen Liu</p>
+                        <p data-value="Lina Gomez"onClick={optionSelect} className='cursor-pointer transition duration-300 hover:text-orange-400'>Lina Gomez</p>
+                    </div>
+                </div>
+            </div>
+            <div className='grid grid-cols-3 gap-4'>
+                <SpinnerBar></SpinnerBar>
+                {selectedRecipe.map(recipe=> {
+                    return(
+                        <div key={recipe.id} className="border border-gray-300 rounded-lg overflow-hidden bg-white">
+                            <div className='relative overflow-hidden'>
+                                <img src={recipe.banner_image} className="h-44 w-full object-cover" />
+                                <div className='absolute bottom-0 bg-gradient-to-t from-black to-transparent px-24 py-12 rotate-45 -translate-x-16 translate-y-12'>
+                                    <div title='Number of Likes' className="flex gap-1 items-center mt-12 -rotate-45">
+                                        <HandThumbUpIcon className='h-5 w-5 text-white' />
+                                        <p className='font-bold text-sm text-white'>{recipe.likes}</p>
+                                    </div>
+                                </div>
+                                <div className='absolute top-0 right-0 bg-gradient-to-b from-black to-transparent px-24 py-12 rotate-45 translate-x-16 -translate-y-12'>
+                                    <div title='Add to Favorites' className="flex gap-1 items-center mb-12 -rotate-45">
+                                        <HeartIcon className={RecipeLikeButtonClasses(recipe)} onClick={(event)=>wantToSave(event, recipe)}/>
+                                    </div>
                                 </div>
                             </div>
-                            <div className='absolute top-0 right-0 bg-gradient-to-b from-black to-transparent px-24 py-12 rotate-45 translate-x-16 -translate-y-12'>
-                                <div title='Add to Favorites' className="flex gap-1 items-center mb-12 -rotate-45">
-                                    <HeartIcon className={RecipeLikeButtonClasses(recipe)} onClick={(event)=>wantToSave(event, recipe)}/>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="p-3">
-                            <p className='font-semibold font-secondary text-xl'>{recipe.name}</p>
-                            <div className='font-semibold font-secondary'>Follow from: {recipe.chef_name.map( chef=> {
-                                    return (
-                                        <button className='px-2 border m-1'>{chef}</button>
-                                    )
-                                }
-                            )}</div>
-                            <p className='font-secondary text-sm'>Rating: <strong>{recipe.rating}</strong></p>
-                            <div className='border border-gray-400 px-2 pt-3 pb-1 mb-2 relative mt-2'>
-                                <p className='absolute top-0 let-0 -translate-y-1/2 -translate-x-1 bg-white px-2'>Ingredients</p>
-                                <ul className='flex flex-wrap'>
+                            <div className="p-4">
+                                <p className='font-semibold font-secondary text-xl'>{recipe.name}</p>
+                                <p className='font-secondary text-sm'>Rating: <strong>{recipe.rating}</strong></p>
+                                <p className='font-semibold font-secondary mt-2'>Follow from:</p>
+                                <div className='flex flex-wrap'> {
+                                    recipe.chef_name.map( chef=> {
+                                        return (
+                                            <Link to={`/chefs?${chef.replace(" ", "_")}`} className='px-2 py-1 border mx-1 my-2 whitespace-nowrap bg-orange-200 text-sm'>{chef}</Link>
+                                        )
+                                    })
+                                }</div>
+                                <p className='mt-2'>Ingredients -</p>
+                                <ul className='list-decimal list-inside mb-2'>
                                     {recipe.ingredients.map(item=>{
                                         return (
                                             <li className='capitalize add_comma text-sm'>{item}</li>
                                         )
                                     })}
                                 </ul>
-                            </div>
-                            <p>Cooking Method: <strong>{recipe.cooking_method}</strong></p>
-                            <p>Cooking Time: <strong>{recipe.cooking_time}</strong></p>
-                            <div className='flex gap-2 mt-2'>
-                                {recipe.tags.map(tag=>{
-                                    return (
-                                        <p className='border border-orange-300 px-2 py-1 text-xs'>{tag}</p>
-                                    )
-                                })}
+                                <p>Cooking Method: <strong>{recipe.cooking_method}</strong></p>
+                                <p>Cooking Time: <strong>{recipe.cooking_time}</strong></p>
+                                <div className='flex gap-2 mt-2'>
+                                    <p>Tags:</p>
+                                    {recipe.tags.map(tag=>{
+                                        return (
+                                            <p className='border border-orange-300 px-2 py-1 text-xs'>{tag}</p>
+                                        )
+                                    })}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </div>
         </div>
     );
 };
