ef778a748c20f7e956cd8feefe4ba570e759ab1d
M. Hasan <mohiuddinkhasherhat@gmail.com>
2023-05-05 23:35:53 +0600

    Added Contact Us page

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import GradientButton from '../Elements/GradientButton';
 
 const Contact = () => {
     return (
         <div>
             <p className="text-center font-bold text-4xl mb-12">Contact Us</p>
             You Can find contact information at the home page.
+            <button onClick="/">
+                <GradientButton text="Go to Home"></GradientButton>
+            </button>
         </div>
     );
 };
